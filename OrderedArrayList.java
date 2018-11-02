a5e021eff583b74e5f88e3605aa60b7ae110d0bd
Tyler Huang <thuang00@stuy.edu>
2018-11-02 11:28:20 -0400

    a

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,4 +5,21 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int capacity){
     super(capacity);
   }
+  private void order(){
+    int count = 0;
+    for (int i = 0; i < super.size()-1; i++){
+      if (super.get(i).compareTo(super.get(i+1)) == 1){
+        swap(i, super.get(i), i+1, super.get(i+1));
+        count++;
+      }
+    }
+    if (count > 0){
+      order();
+    }
+  }
+  private void swap(int idx1, T obj1, int idx2, T obj2){
+    super.set(idx1, obj1);
+  }
 }
+
+//["1", "3", "2", "5", "6"]
